6041527e91f196491cbdaf3eda654cc7ef3c6288
Knugi <24708955+knugi0123@users.noreply.github.com>
2018-06-03 20:22:05 +0800

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RandomKeyGenerator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Your Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Key Length:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "The key will pasted to Clipboard automatically\r\nwhen you click \"Get Your Key\".";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 189);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Random Key Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using mpfstudio;
+
+namespace RandomKeyGenerator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+            private static RNGCryptoServiceProvider KeyGenCore = new RNGCryptoServiceProvider();
+            public static string GetUniqueKey(int maxSize)
+            {
+                
+                char[] charactors = new char[81];
+                charactors = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#^,.?!|&_~@$%/\=+-*".ToCharArray();
+                byte[] data = new byte[1];
+                try
+                {
+                    KeyGenCore.GetNonZeroBytes(data);
+                    data = new byte[maxSize];
+                    KeyGenCore.GetNonZeroBytes(data); 
+                }
+                catch (OutOfMemoryException)
+                {
+                    MessageBox.Show("Computer do not have enough memory for calculating or save the results.", "Out Of Memory");
+                }
+                StringBuilder ResultKey = new StringBuilder(maxSize);
+                foreach (byte b in data)
+                {
+                    ResultKey.Append(charactors[b % (charactors.Length)]);
+                }
+                return ResultKey.ToString();
+            } 
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox2.Text, out int parsedValue))
+            {
+                MessageBox.Show("Key Lenght must be integer", "Warning");
+                return;
+            }
+            else
+            {
+                //int Keylenght = int.Parse(textBox2.Text);
+                //string Key = GetUniqueKey(Keylenght);
+                richTextBox1.Text = GetUniqueKey(int.Parse(textBox2.Text));
+                Clipboard.SetText(richTextBox1.Text);
+            }
+        }
+    }
+}
